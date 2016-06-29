46b144fed345aaa7ee8df03dd2fe4544bfbacbc3
Naja Melan <najamelan@autistici.org>
2016-06-29 18:00:13 +0200

    Make submodules optional, but loaded if present

--- lib/tidbits.rb
+++ lib/tidbits.rb
@@ -1,5 +1,28 @@
-require_relative 'core_extend/lib/core_extend'
-require_relative 'git/lib/git'
-require_relative 'options/lib/options'
-require_relative 'rush/lib/rush'
-require_relative 'susu/lib/susu'
+# TidBits is a library of snippets.
+# Sometimes users my not want to check out all snippets (git submodules), so here
+# load everything that does exist, but don't barf if some files don't.
+#
+module TidBits
+
+[
+	  { name: 'CoreExtend', file: 'core_extend/lib/core_extend'  } \
+	, { name: 'Git'       , file: 'git/lib/git'                  } \
+	, { name: 'Options'   , file: 'options/lib/options'          } \
+	, { name: 'Rush'      , file: 'rush/lib/rush'                } \
+	, { name: 'Susu'      , file: 'susu/lib/susu'                } \
+
+].each do |lib|
+
+	begin
+
+		require_relative lib[ :file ]
+
+	rescue LoadError
+
+		puts "Submodule #{ lib[ :name ] } not present."
+
+	end
+
+end
+
+end # module TidBits
