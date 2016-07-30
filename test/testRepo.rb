9f60a7eed730e0d9e30c9bfcf0f99d7df962da70
Naja Melan <najamelan@autistici.org>
2016-07-30 23:28:07 +0200

    Add some basic unit tests

--- test/testRepo.rb
+++ test/testRepo.rb
@@ -6,9 +6,27 @@ class TestRepo < TestRepoCase
 
 def test00Constructor
 
-	r = Repo.new @@clean
+	r = Repo.new @@repo
 
-	assert_instance_of  Repo	, r
+	assert_instance_of  Repo  , r
+	assert_equal        @@repo, r.path
+
+	assert                r.valid?
+	assert                r.workingDirClean?
+	assert              ! r.bare?
+
+end
+
+
+def test01CleanWorkingDir
+
+	r = Repo.new @@repo
+
+	assert r.workingDirClean?
+
+	pollute r.path
+
+	assert ! r.workingDirClean?
 
 end
 
