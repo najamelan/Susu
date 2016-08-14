409159366ffb41d0db8ddf787e08e42ff52a453f
Naja Melan <najamelan@autistici.org>
2016-08-14 23:43:16 +0200

    Use symbols in autoload

--- lib/susu/fs.rb
+++ lib/susu/fs.rb
@@ -2,8 +2,8 @@
 module Susu
 module Fs
 
-	autoload 'Refine', File.join( __dir__, 'fs/refine' )
-	autoload 'Path'  , File.join( __dir__, 'fs/path'   )
+	autoload :Refine, File.join( __dir__, 'fs/refine' )
+	autoload :Path  , File.join( __dir__, 'fs/path'   )
 
 end # module Fs
 end # module Susu
--- lib/susu/options.rb
+++ lib/susu/options.rb
@@ -3,11 +3,11 @@ require 'hashie'
 module Susu
 module Options
 
-	autoload 'Refine'        , File.join( __dir__, 'options/refine'        )
-	autoload 'Configurable'  , File.join( __dir__, 'options/configurable'  )
-	autoload 'Settings'      , File.join( __dir__, 'options/settings'      )
-	autoload 'Config'        , File.join( __dir__, 'options/config'        )
-	autoload 'ConfigProfile' , File.join( __dir__, 'options/configProfile' )
+	autoload :Refine        , File.join( __dir__, 'options/refine'        )
+	autoload :Configurable  , File.join( __dir__, 'options/configurable'  )
+	autoload :Settings      , File.join( __dir__, 'options/settings'      )
+	autoload :Config        , File.join( __dir__, 'options/config'        )
+	autoload :ConfigProfile , File.join( __dir__, 'options/configProfile' )
 
 end # module Options
 end # module Susu
