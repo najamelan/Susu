f60d07100fadb288a0cc982693059ac1f57a3980
Naja Melan <najamelan@autistici.org>
2016-11-16 15:20:52 +0100

    whitespace

--- test/git/testFactRepo.rb
+++ test/git/testFactRepo.rb
@@ -13,13 +13,13 @@ def test00DependOnPath
 
 	f = Facts::Repo.new( path: path, bare: false )
 
-	assert !path.exist?
+		assert !path.exist?
 
-	assert_analyze_fail f
+		assert_analyze_fail f
 
-	assert_fix          f
-	assert              path          .directory?
-	assert              path[ '.git' ].directory?
+		assert_fix          f
+		assert              path          .directory?
+		assert              path[ '.git' ].directory?
 
 end
 
@@ -31,42 +31,47 @@ def test01Exist
 
 	f = Facts::Repo.new( path: @@repo )
 
-	assert_analyze f
-	assert_check   f
+		assert_analyze f
+		assert_check   f
 
 
 	# Remove the repo
 	#
 	g = Facts::Repo.new( path: @@repo, exist: false )
 
-	assert_check_fail g
-	assert_fix        g
+		assert_check_fail g
+		assert_fix        g
 
-	assert            ! @@repo[ '.git' ].exist?
+		assert            ! @@repo[ '.git' ].exist?
 
 
 	# Recreate it
 	#
 	f.reset
 
-	assert_check_fail f
+		assert_check_fail f
+
 
 	h = Facts::Repo.new( path: @@repo, bare: false )
 
-	assert_fix   h
-	assert       @@repo[ '.git' ].exist?
-	assert       @@repo[ '.git' ].directory?
-	assert     ! h.repo.bare?
+		assert_fix   h
+		assert       @@repo[ '.git' ].exist?
+		assert       @@repo[ '.git' ].directory?
+		assert     ! h.repo.bare?
+
 
 	# Test create bare
 	#
 	h = Facts::Repo.new( path: @@repo, exist: false )
-	assert   h.fix
-	assert ! @@repo[ '.git' ].exist?
+
+		assert   h.fix
+		assert ! @@repo[ '.git' ].exist?
+
 
 	h = Facts::Repo.new( path: @@repo, bare: true )
-	assert h.fix
-	assert h.repo.bare?
+
+		assert h.fix
+		assert h.repo.bare?
 
 	# Create in non empty directory
 	#
@@ -75,12 +80,12 @@ def test01Exist
 
 	g = Facts::Repo.new( path: path, bare: false )
 
-	assert_analyze_fail g
-	assert_fix          g
+		assert_analyze_fail g
+		assert_fix          g
 
-	assert              path[ '.git' ].exist?
-	assert              path[ '.git' ].directory?
-	assert              Repo.new( path ).valid?
+		assert              path[ '.git' ]  .exist?
+		assert              path[ '.git' ]  .directory?
+		assert              Repo.new( path ).valid?
 
 
 	# Create in non empty directory, shoouldn't if force is not set
@@ -90,16 +95,16 @@ def test01Exist
 
 	g = Facts::Repo.new( path: path, bare: false, force: false )
 
-	assert_analyze_fail g
+		assert_analyze_fail g
 
-	assert_raise Rugged::FilesystemError do
+		assert_raise Rugged::FilesystemError do
 
 		g.fix
 
 	end
 
-	assert              path[ '.git' ].exist?
-	assert            ! Repo.new( path ).valid?
+		assert    path[ '.git' ]  .exist?
+		assert  ! Repo.new( path ).valid?
 
 end
 
@@ -108,25 +113,25 @@ def test02Bare
 
 	f = Facts::Repo.new( path: @@repo, bare: false )
 
-	assert_analyze f
-	assert_check   f
+		assert_analyze f
+		assert_check   f
 
 	# Change to bare
 	#
 	f.reset
 	g = Facts::Repo.new( path: @@repo, bare: true )
 
-	assert_check_fail g
-	assert_fix        g
-	assert_check      g
-	assert_check_fail f
-	assert            g.repo.bare?
+		assert_check_fail g
+		assert_fix        g
+		assert_check      g
+		assert_check_fail f
+		assert            g.repo.bare?
 
 
-	# Change back to non bare
-	#
-	assert_fix   f
-	assert     ! f.repo.bare?
+		# Change back to non bare
+		#
+		assert_fix   f
+		assert     ! f.repo.bare?
 
 end
 
@@ -135,19 +140,19 @@ def test03Update
 
 	f = Facts::Repo.new( path: @@repo, update: true )
 
-	assert_analyze f
-	assert_check   f
+		assert_analyze f
+		assert_check   f
 
 
 	f = Facts::Repo.new( path: @@repo, update: true )
 
-	f.repo.pollute
-	assert ! f.repo.clean?
+		f.repo.pollute
+		assert ! f.repo.clean?
 
-	assert_analyze    f
-	assert_check_fail f
-	assert_fix        f
-	assert_check      f
+		assert_analyze    f
+		assert_check_fail f
+		assert_fix        f
+		assert_check      f
 
 end
 
